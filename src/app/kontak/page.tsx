57c29b0b7bab0bf7f7d8742fa372e1c6a8de908b
Erhaes <rassyahafizh@gmail.com>
2025-09-08 20:51:36 +0700

    memperbaiki

--- src/app/kontak/page.tsx
+++ src/app/kontak/page.tsx
@@ -1,4 +1,3 @@
-import ContactFAQ from "@/components/Contact/ContactFAQ";
 import ContactHeader from "@/components/Contact/ContactHeader";
 import ContactMain from "@/components/Contact/ContactMain";
 
@@ -11,7 +10,6 @@ export default function ContactPage() {
     <main>
       <ContactHeader />
       <ContactMain />
-      <ContactFAQ />
     </main>
   );
 }
